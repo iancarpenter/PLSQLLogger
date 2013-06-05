80502cde668d5f7b8ba3eb741154d336555136be
Ian Carpenter <ianhcarpenter@gmail.com>
2013-06-05 22:33:15 +0100

    Refactored columns.
    Added a column that records when the debug was inserted.
    
    Added a column whose use will be to determine how much debug to record
    
    Added Primary Key constraint

--- ianc_debug_tbl.sql
+++ ianc_debug_tbl.sql
@@ -1,9 +1,11 @@
 CREATE TABLE IANC_DEBUG_TBL
 (	
-   ICD_ID       NUMBER(18), 
-   VARCHAR2_COL VARCHAR2(4000), 
-   NUMBER_COL   NUMBER, 
-   DATE_COL     DATE
+   id            NUMBER(18), 
+   text          VARCHAR2(4000), 
+   time_stamp    TIMESTAMP,
+   debug_level   NUMBER(1),
+   CONSTRAINT logger_logs_pk PRIMARY KEY (id) ENABLE
 )
 /
+
   
