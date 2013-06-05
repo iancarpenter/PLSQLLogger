f67796b8ead8132a50e4d7ba0ce9e58cf6299b6c
Ian Carpenter <ianhcarpenter@gmail.com>
2013-06-05 22:40:36 +0100

    Refactored remaining L procedure
    Changed the L(og) procedure to match the new debug table structure.

--- ianc_debug_body.sql
+++ ianc_debug_body.sql
@@ -3,33 +3,21 @@ IS
 --------------------------------------------------------------------------------
    PROCEDURE l 
    (
-      pc_text   IN VARCHAR2, 
-      pc_number IN NUMBER,
-      pc_date   IN DATE
-   )
-   IS
-      PRAGMA AUTONOMOUS_TRANSACTION;
-   BEGIN
-      
-      INSERT INTO ianc_debug_tbl 
-      VALUES(ianc_debug_seq.NEXTVAL, pc_text, pc_number, pc_date);
- 
-      COMMIT;
- 
-   END l;
---------------------------------------------------------------------------------
-   PROCEDURE l 
-   (
       pc_text IN VARCHAR2
    )
    IS
       PRAGMA AUTONOMOUS_TRANSACTION;
    BEGIN
          
-      INSERT INTO ianc_debug_tbl 
-      VALUES(ianc_debug_seq.NEXTVAL, pc_text, NULL, NULL);
+      INSERT INTO ianc_debug_tbl(id, 
+                                 text,
+                                 time_stamp)								 
+      VALUES(ianc_debug_seq.NEXTVAL, 
+	         pc_text, 
+			 current_timestamp);
     
       COMMIT;
+	  
    END l;
 --------------------------------------------------------------------------------
 END ianc_debug;
