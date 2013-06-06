7640a3cf675d5740fcc7fa57eb3a1f327c5a55c6
Ian Carpenter <ianhcarpenter@gmail.com>
2013-06-06 22:18:56 +0100

    Formatting
    No code\functionality changes

--- ianc_debug_body.sql
+++ ianc_debug_body.sql
@@ -9,12 +9,18 @@ IS
       PRAGMA AUTONOMOUS_TRANSACTION;
    BEGIN
          
-      INSERT INTO ianc_debug_tbl(id, 
-                                 text,
-                                 time_stamp)								 
-      VALUES(ianc_debug_seq.NEXTVAL, 
-	         pc_text, 
-			 current_timestamp);
+      INSERT INTO ianc_debug_tbl
+	  (
+	     id, 
+         text,
+         time_stamp
+	  )								 
+      VALUES
+	  (
+	     ianc_debug_seq.NEXTVAL, 
+	     pc_text, 
+		 current_timestamp
+	  );
     
       COMMIT;
 	  
