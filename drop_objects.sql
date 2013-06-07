d1926c31306f43b43541cc56b4ef4916a6ae448a
Ian Carpenter <ianhcarpenter@gmail.com>
2013-06-07 22:16:22 +0100

    Refactored
    No Longer Required

--- drop_objects.sql
+++
@@ -1,6 +0,0 @@
-DROP TABLE ianc_debug_tbl
-/
-DROP SEQUENCE ianc_debug_seq
-/
-DROP PACKAGE ianc_debug
-/
--- ianc_debug_vw.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE VIEW ianc_debug_vw
-AS
-SELECT idt.ID, 
-       idt.text,
-       idt.time_stamp
-  FROM ianc_debug_tbl idt
- ORDER BY idt.time_stamp desc
-/
